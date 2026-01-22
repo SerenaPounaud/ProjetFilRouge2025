77c393bd4374507d3ed1805485a5f0284ab6d103
Séréna <Serena.Pounaud@outlook.fr>
2026-01-22 18:08:51 +0100

    En cours : afficher message d'erreur

--- frontend/assets/js/contact.js
+++ frontend/assets/js/contact.js
@@ -1,69 +1,65 @@
-const formContact = document.getElementById("form");
+const formcontact = document.getElementById("form");
 
-function showError (message, errorDivId, inputIds = []) {
-    const errorDiv = document.getElementById(errorDivId);
+function showError (message, fieldId) {
+    const errorDiv = document.getElementById(fieldId + "_error");
     if (!errorDiv) return;
 
-    //affiche le message d'erreur en rouge
     errorDiv.textContent = message;
-    errorDiv.classList.add("error");
+    errorDiv.style.opacity = "1";
     errorDiv.style.display = "block";
 
-    //applique bordure rouge aux champs concernés
-    inputIds.forEach(id => {
-      const input = document.getElementById(id);
-      if (input) input.classList.add("input-error");
-    });
+    //applique bordure rouge
+    const input = document.getElementById(fieldId);
+    if (input) input.classList.add("input-error");
 
     //masque l'erreur après 3s
     setTimeout(() => {
-        errorDiv.classList.remove("error");
+        errorDiv.style.opacity = "0";
         errorDiv.style.display = "none";
-        inputIds.forEach(id => {
-          const input = document.getElementById(id);
-          if (input) input.classList.remove("input-error");
-        });
+        if (input) input.classList.remove("input-error");
     }, 3000);
 }
 
-if (formContact) {
-    formContact.addEventListener("submit", (e) => {
-    e.preventDefault();
+if (formcontact) {
+    formcontact.addEventListener("submit", (e) => {
+      e.preventDefault();
 
     const nom = document.getElementById("lastname").value.trim();
     const prenom = document.getElementById("firstname").value.trim();
     const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
     const rgpd = document.getElementById("rgpd").checked;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!nom) {
-        showError ("Veuillez entrer votre nom", "contact_error", ["lastname"]);
+        showError ("Veuillez entrer votre nom", "lastname");
         return;
     }
      if (!prenom) {
-        showError("Veuillez entrer votre prénom", "contact_error", ["firstname"]);
+        showError("Veuillez entrer votre prénom", "firstname");
         return;
     }
-    if (!email.includes("@")) {
-        showError("Email invalide Ex: exemple@gmail.fr", "contact_error", ["email"]);
+    if (!emailRegex.test(email)) {
+        showError("Email invalide Ex: exemple@gmail.fr", "email");
+        return;
+    }
+    if (!message) {
+        showError("Veuillez écrire votre message", "message");
         return;
     }
     if (!rgpd) {
-        showError("Veuillez consentir au traitement de vos données personnelles", "contact_error", ["rgpd"]);
+        showError("Veuillez consentir au traitement de vos données personnelles","rgpd");
         return;
     }
-
     toast("Connexion réussie !");
-   formContact.reset();
+    formcontact.reset();
 });
 }
 
-fetch("../templates/header.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
-    initHeaderSearch();
-});
+  fetch("../templates/header.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("header").innerHTML = data);
 
-fetch("../templates/footer.html")
-  .then(res => res.text())
-  .then(data => document.getElementById("footer").innerHTML = data);
+    fetch("../templates/footer.html")
+      .then(res => res.text())
+      .then(data => document.getElementById("footer").innerHTML = data);
