f308243a116fddd17550d3d0597f47a1f7f2eb7f
Séréna <Serena.Pounaud@outlook.fr>
2026-03-02 21:53:55 +0100

    Suppression chargement footer d'index

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -272,7 +272,3 @@ document.addEventListener('DOMContentLoaded', async () => { //attend que html so
 
     displayRecipesPage(1, filteredMeals);
 });
-
-fetch("templates/footer.html")
-    .then(res => res.text())
-    .then(data => document.getElementById("footer").innerHTML = data);
