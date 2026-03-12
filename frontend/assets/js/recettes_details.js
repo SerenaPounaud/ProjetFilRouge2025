a4669e32a7723de1493c3c58bec9446f5ad1b9df
Séréna <Serena.Pounaud@outlook.fr>
2026-03-12 22:06:42 +0100

    Ajout css header de details recette

--- frontend/assets/js/recettes_details.js
+++ frontend/assets/js/recettes_details.js
@@ -12,7 +12,7 @@ if (!idMeal) { //vérifie si idMeal est vide ou null et affiche un message si c'
     try { //try pour gérer les erreurs
       const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`); //récupère les détails de la recette correspondant à l'id
       const data = await response.json(); //transforme la réponse en objet js via .json
-      return data.meals ? data.meals[0] : null; //vérifie si data.meals existe, si oui renvoie le premier élément du tableau meals
+      return data.meals ? data.meals[0] : null; //si data.meals existe, si oui renvoie le premier élément du tableau meals
     } catch (error) {
       console.error('Erreur lors de la récupération de la recette:', error);
       return null; //renvoie null si aucune recette a été recupérée
@@ -21,7 +21,7 @@ if (!idMeal) { //vérifie si idMeal est vide ou null et affiche un message si c'
 
 // Affiche des recettes similaires  
 function displayRecettesSimilaire(meals, currentId) {
-  divSimilaires.innerHTML = ""; //vide la div pour afficher de nouvelles recettes/mise à jout
+  divSimilaires.innerHTML = ""; //vide la div pour afficher de nouvelles recettes/mise à jour
 
   meals.forEach(meal => {
     if (meal.idMeal === currentId) return; //évite d'afficher la recette actuelle
@@ -68,9 +68,9 @@ function fetchRecettesSimilaires(category, currentId) {
 
 // Afficher la recette
 getRecipeDetails(idMeal).then(meal => { //appelle la fonction, meal = recevra le résultat
-  if (!meal) { //si null ou undefined
+  if (!meal) {
     recipeDetailsDiv.innerHTML = '<p>Recette introuvable.</p>';
-    return; //quitte la fonction pour éviter que le code continu si meal est absent
+    return;
   }
   fetchRecettesSimilaires(meal.strCategory, meal.idMeal);
   
@@ -152,27 +152,41 @@ const noteHtml = noteBlock.outerHTML;
 
 recipeDetailsDiv.innerHTML = `
 
-<h2>${meal.strMeal || '—'}</h2>
-<p class='date-recette'>Publiée le <strong>${dateFormatted}</strong></p>
-<img src="${meal.strMealThumb || ''}" alt="${meal.strMeal || ''}" width="300">
-<div>
-  <p><strong>Temps de cuisson :</strong> ${afficheTemps}</p>
-  <p><strong>Pour :</strong> ${personnes} personne(s)</p>
-</div>
-  <div class="note">
-    ${noteHtml}
-    <button type= "button" class="btnDetails" title="Ajouter aux favoris" aria-label="Ajouter aux favoris" style="background: transparent; border: none; padding: 0; cursor: pointer;">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="40" height="40">
-      <path d="M378.9 64c-32.4 0-62.9 15.6-81.9 41.9l-28 38.7c-3 4.2-7.8 6.6-13 6.6s-10-2.5-13-6.6l-28-38.7c-19-26.3-49.5-41.9-81.9-41.9-55.9 0-101.1 45.3-101.1 101.1 0 55 34.4 107.1 71.8 152.5 42.1 51.2 93.4 96 128.5 122.9 3.8 2.9 8.4 5.1 13.4 6.3 2.9 11.5 6.8 22.6 11.7 33.2-.5 0-.9 0-1.4 0-15.6 0-30.8-4.6-43.1-14.1-36.5-27.9-89.7-74.4-133.8-127.9-37.5-45.5-79.1-105.5-79.1-172.8 0-73.5 59.6-133.1 133.1-133.1 42.7 0 82.8 20.5 107.9 55.1l15 20.7 15-20.7c25-34.6 65.2-55.1 107.9-55.1 73.5 0 133.1 59.6 133.1 133.1 0 19.4-3.5 38.3-9.3 56.3-9.8-3.9-20.1-7-30.6-9.2 5-15.4 7.9-31.1 7.9-47.1 0-55.9-45.3-101.1-101.1-101.1zM544 400a112 112 0 1 0 -224 0 112 112 0 1 0 224 0zm-256 0a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm160-64l0 48 48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-48 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48-48 0c-8.8 0-16-7.2-16-16s7.2-16 16-16l48 0 0-48c0-8.8 7.2-16 16-16s16 7.2 16 16z" fill="red"/>
-      </svg>
-    </button>
-    <button type= "button" class="btnDetails" title ="Imprimer la recette" aria-label="Imprimer la recette" style="background: transparent; border: none; padding: 0; cursor: pointer;">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="40" height="40"><!--!Font Awesome Pro v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.-->
-      <path d="M64 64C64 28.7 92.7 0 128 0L341.5 0c17 0 33.3 6.7 45.3 18.7l42.5 42.5c12 12 18.7 28.3 18.7 45.3l0 37.5-384 0 0-80zM0 256c0-35.3 28.7-64 64-64l384 0c35.3 0 64 28.7 64 64l0 96c0 17.7-14.3 32-32 32l-32 0 0 64c0 35.3-28.7 64-64 64l-256 0c-35.3 0-64-28.7-64-64l0-64-32 0c-17.7 0-32-14.3-32-32l0-96zM128 416l0 32 256 0 0-96-256 0 0 64zM456 272a24 24 0 1 0 -48 0 24 24 0 1 0 48 0z"/>
-      </svg>
-    </button>
+<div class="headerDetails">
+  <div class="imgDetails">
+    <img src="${meal.strMealThumb || ''}" alt="${meal.strMeal || ''}">
   </div>
-    
+  <div class="infoDetails">
+    <h2>${meal.strMeal || '—'}</h2>
+    <div>
+      <p class="loremDetails">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis debitis sint dignissimos vitae voluptates hic harum voluptas illo totam soluta quidem temporibus, laborum maxime nihil facilis voluptatibus sapiente quae! Odio?
+      Cupiditate vitae consequuntur eos accusantium, fugit unde totam ut facere voluptatem error.</p>
+      <div>
+        <p>Publiée le <strong>${dateFormatted}</strong></p>
+        <p><strong>Temps de cuisson :</strong> ${afficheTemps}</p>
+        <p><strong>Pour :</strong> ${personnes} personne(s)</p>
+      </div>
+    </div>
+
+    <div class="noteDetails">
+      ${noteHtml}
+      <div class="buttonDetails">
+        <button type= "button" class="btnDetails" title="Ajouter aux favoris" aria-label="Ajouter aux favoris">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path d="M378.9 64c-32.4 0-62.9 15.6-81.9 41.9l-28 38.7c-3 4.2-7.8 6.6-13 6.6s-10-2.5-13-6.6l-28-38.7c-19-26.3-49.5-41.9-81.9-41.9-55.9 0-101.1 45.3-101.1 101.1 0 55 34.4 107.1 71.8 152.5 42.1 51.2 93.4 96 128.5 122.9 3.8 2.9 8.4 5.1 13.4 6.3 2.9 11.5 6.8 22.6 11.7 33.2-.5 0-.9 0-1.4 0-15.6 0-30.8-4.6-43.1-14.1-36.5-27.9-89.7-74.4-133.8-127.9-37.5-45.5-79.1-105.5-79.1-172.8 0-73.5 59.6-133.1 133.1-133.1 42.7 0 82.8 20.5 107.9 55.1l15 20.7 15-20.7c25-34.6 65.2-55.1 107.9-55.1 73.5 0 133.1 59.6 133.1 133.1 0 19.4-3.5 38.3-9.3 56.3-9.8-3.9-20.1-7-30.6-9.2 5-15.4 7.9-31.1 7.9-47.1 0-55.9-45.3-101.1-101.1-101.1zM544 400a112 112 0 1 0 -224 0 112 112 0 1 0 224 0zm-256 0a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm160-64l0 48 48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-48 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48-48 0c-8.8 0-16-7.2-16-16s7.2-16 16-16l48 0 0-48c0-8.8 7.2-16 16-16s16 7.2 16 16z" fill="red"/>
+          </svg>
+        </button>
+        <button type= "button" class="btnDetails" title ="Imprimer la recette" aria-label="Imprimer la recette">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Pro v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2025 Fonticons, Inc.-->
+            <path d="M64 64C64 28.7 92.7 0 128 0L341.5 0c17 0 33.3 6.7 45.3 18.7l42.5 42.5c12 12 18.7 28.3 18.7 45.3l0 37.5-384 0 0-80zM0 256c0-35.3 28.7-64 64-64l384 0c35.3 0 64 28.7 64 64l0 96c0 17.7-14.3 32-32 32l-32 0 0 64c0 35.3-28.7 64-64 64l-256 0c-35.3 0-64-28.7-64-64l0-64-32 0c-17.7 0-32-14.3-32-32l0-96zM128 416l0 32 256 0 0-96-256 0 0 64zM456 272a24 24 0 1 0 -48 0 24 24 0 1 0 48 0z"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+  </div>
+</div> 
+ 
 <div class="middleDetails">
   <div>
     <h3>Instructions :</h3>
