346934763368fc084130c5233a9599146e779ebc
Séréna <Serena.Pounaud@outlook.fr>
2026-04-15 22:04:26 +0200

    Css responsive connexion

--- frontend/assets/js/toast.js
+++ frontend/assets/js/toast.js
@@ -21,6 +21,6 @@ function toast(message) {
     toastTimeout = setTimeout(() => {
         t.style.opacity = "0";
         t.style.transform = "translateY(20px)"; //déplace vers le bas
-        t.style.zIndex = "10";
+        t.style.zIndex = "20";
     }, 3000);
 }  
