ce886fdb0bf87c59618df59610cb475bd867af3a
Séréna <Serena.Pounaud@outlook.fr>
2026-05-06 21:56:58 +0200

    Ajout composant home

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,6 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication } from '@angular/platform-browser'; //sert à démarrer l'appli angular
+import { appConfig } from './app/app.config';  //config global
+import { App } from './app/app'; //composant principal
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+//démarre l'application
