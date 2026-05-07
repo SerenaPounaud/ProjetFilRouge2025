956b6410cacd776baac2ef8ac12c8155fb362150
Séréna <Serena.Pounaud@outlook.fr>
2026-05-07 12:08:30 +0200

    Ajout chemin header, footer dans app

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './components/home/home';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
