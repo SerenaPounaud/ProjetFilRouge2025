8d0912523ffa9f371b2741e659a54af7b36643f4
Séréna <Serena.Pounaud@outlook.fr>
2026-05-07 20:27:23 +0200

    Ajout connexion, inscription et profil + css et routerlink

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Contact } from './components/contact/contact';
+import { Profil } from './components/profil/profil';
+import { SignIn } from './components/sign-in/sign-in';
+import { SignUp } from './components/sign-up/sign-up';
 
 export const routes: Routes = [
     {path: '', component:Home},
     {path: 'contact', component:Contact},
+    {path: 'profil', component:Profil},
+    {path: 'sign-in', component:SignIn},
+    {path: 'sign-up', component:SignUp},
 ];
