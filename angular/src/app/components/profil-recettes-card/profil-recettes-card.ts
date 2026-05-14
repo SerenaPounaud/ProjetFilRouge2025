9514e0904017daa79085240f9abf8e0b9d78a5f9
Séréna <Serena.Pounaud@outlook.fr>
2026-05-14 15:21:13 +0200

    Ajout/modification angular material icons

--- angular/src/app/components/profil-recettes-card/profil-recettes-card.ts
+++ angular/src/app/components/profil-recettes-card/profil-recettes-card.ts
@@ -1,18 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-profil-recettes-card',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './profil-recettes-card.html',
   styleUrl: './profil-recettes-card.css',
 })
 export class ProfilRecettesCard {
   @Input() element:any;
   @Input() index!: number; //position de la recette
-  @Output() delete = new EventEmitter<number>(); //event suppression enfant -> parent
+  @Output() deleteRecette = new EventEmitter<number>(); //event suppression enfant -> parent
 
   removeRecetteUser(): void { //envoi l'index au parent
-    this.delete.emit(this.index);
+    this.deleteRecette.emit(this.index);
   }
 
 }
