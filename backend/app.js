2ce3a9236b29def26d57b57bdcfbf6ddf83f6e9d
Séréna <Serena.Pounaud@outlook.fr>
2026-05-20 18:01:54 +0200

    Suppression prisma

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 //configuration express
 const express = require('express');
+const userRoutes = require("./routes/user.routes");
+
 const app = express();
+
+//connexion routes
+app.use("/api", userRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const connecDB = require("./config/db");
 
-const PORT = 3306; //mysql
+connecDB(); //lance la connexion vers la bd
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Le serveur tourne sur le port ${PORT}`);
