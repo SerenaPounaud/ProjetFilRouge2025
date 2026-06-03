8d8497a7e517675e73d071372d2aeec81d53420e
Séréna <Serena.Pounaud@outlook.fr>
2026-06-03 13:17:43 +0200

    Commentaires

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken"; //permet de générer un token lors d'une connexion
+import jwt from "jsonwebtoken"; //permet de créer et vérifier un token lors d'une connexion
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization; //récupère le token
+    const authHeader = req.headers.authorization; //récupère l'en-tête http
     
     if(!authHeader) return res.status(403).json({message: "Token requis"});
 
-    const token = authHeader.split(" ")[1]; //retourne uniquement le token (Bearer "Token", après l'espace)
+    const token = authHeader.split(" ")[1]; //retourne uniquement le token (Bearer "Token")
 
     try { //vérifie la validité du token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); //récupère la clé pour la sécurité
-        req.userId = decoded.id;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET); //vérifie qu'il est valide
+        req.userId = decoded.id; //ajoute l'id de l'user à la requête
         next();
     } catch (error) {
         return res.status(401).json({message: "Token invalide"})
