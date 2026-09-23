62ff9f9d5e19a446e187f783d5e73c8a06421e2b
Séréna <Serena.Pounaud@outlook.fr>
2026-09-23 16:57:25 +0200

    Ajout html recette détails

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -7,5 +7,5 @@ export const routes: Routes = [
     {path: 'contact', loadComponent:() => import('./components/contact/contact').then(m => m.Contact)},
     {path: 'profil', canActivate: [authGuard], loadComponent:() => import('./components/profil/profil').then(m => m.Profil)},
     {path: 'sign-in', loadComponent:() => import('./components/sign-in/sign-in').then(m => m.SignIn)},
-    {path: 'sign-up', loadComponent:() => import('./components/sign-up/sign-up').then(m => m.SignUp)},
+    {path: 'sign-up', loadComponent:() => import('./components/sign-up/sign-up').then(m => m.SignUp)}
 ];
